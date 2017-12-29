d4c6125ae96740dfb0bd2efe058f36ef99483e48
saumya <saumyasinha023@gmail.com>
2017-12-28 19:43:59 -0800

    few new programs from leetcode

--- Kreverselinkedlist.py
+++ Kreverselinkedlist.py
@@ -1,31 +1,40 @@
-class Node():
+class ListNode():
     def __init__(self, head):
         self.val = head
         self.next = None
 
 
-first = Node(1)
-first.next = Node(2)
-first.next.next = Node(3)
-first.next.next.next = Node(4)
-first.next.next.next.next = Node(5)
+first = ListNode(1)
+
+first.next = ListNode(2)
 
 
-class Solution(object):
-    def reverseList(self, A, B):
-        head = A
-        C=Node(0)
-        count = 1
-        while count < B:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-            count += 1
-            if count==B-1:
-                A = A.next.next
-                C.next=A.next
-                count-=1
+class Solution():
 
+    def reverseKGroup(self, head, k):
+        dummy = jump = ListNode(0)
+        dummy.next = l = r = head
+
+        while True:
+            count = 0
+            while r and count < k:  # use r to locate the range
+                r = r.next
+                count += 1
+            if count == k:  # if size k satisfied, reverse the inner linked list
+                pre, cur = r, l
+                for _ in range(k):
+                    cur.next, cur, pre = pre, cur.next, cur  # standard reversing
+                jump.next, jump, l = pre, l, r  # connect two k-groups
             else:
-                A=A.next
+                return dummy.next
+
 
 S = Solution()
-S.reverseList(first, 3)
+head1 = S.reverseKGroup(first, 2)
+print(head1.val, head1.next.val)
--- tmp.py
+++ tmp.py
@@ -1,11 +1,33 @@
-class Solution:
-    def rotate(self, A):
-        A[:] = map(list,zip(*A[::-1]))
-        return A
-A=[
-  [1,2,3],
-  [4,5,6],
-  [7,8,9]
-]
-S = Solution()
-print(S.rotate(A))
+import time
+
+num = 1
+den = 97
+graph = {}
+
+
+def calculate(num, den):
+    count = 0
+    while True:
+        count += 1
+        rem = num % den
+        if rem in graph:
+
+            return (len(graph), count)
+        elif rem == 0:
+            return 0, count
+        else:
+            graph[rem] = 1
+            num = rem * 10
+
+
+maxim = 0
+start = time.time()
+steps = 0
+for i in range(1, 10000):
+    for j in range(1, 10000):
+        maxim = max(maxim, calculate(i, j)[0])
+        steps = steps + calculate(i, j)[1]
+end = time.time()
+print(steps)
+print(maxim)
+print(end - start)
